5942c4067ea139c6a93bfeb0da392ba9d8c8782c
Yingjie Zhong <zhong1@stolaf.edu>
2022-07-10 19:40:09 -0500

    Generate JWT

---
+++ backend/controllers/userController.js
@@ -0,0 +1,79 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
+// Register new user
+// POST /api/users
+// Public access
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+
+    if (!name || !email || !password) {
+        res.status(400)
+        throw new Error('Please add all fields')
+    }
+
+    //Check if user exist
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    //Hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+
+})
+
+// Authenticate a user
+// POST /api/users/login
+// Public access
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body
+
+    const user = await User.findOne({ email })
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    }else{
+        res.status(400)
+        throw new Error('Invalid credentials')
+    }
+})
+
+// Register new user
+// GET /api/users/me
+// Public access
+const getMe = asyncHandler(async (req, res) => {
+    res.json({ message: 'User data display' })
+})
+
+module.exports = {
+    registerUser,
+    loginUser,
+    getMe,
+}
