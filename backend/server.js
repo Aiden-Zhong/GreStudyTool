b9d3b0b4de7159ab5e928dab45575b3e7c9f0fdd
Yingjie Zhong <zhong1@stolaf.edu>
2022-07-21 01:35:41 -0500

    added register function

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,14 @@ const dotenv = require('dotenv').config()
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
 const {errorHandler} = require('./middleware/errorMiddleware')
+const cors = require('cors');
 
 connectDB()
 
 const app = express()
+
+
+app.use(cors());
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
